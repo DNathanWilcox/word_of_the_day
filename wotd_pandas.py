0035feb8601fce4083f2033836f15e715ca3b780
Nathan Wilcox <37597253+DNathanWilcox@users.noreply.github.com>
2022-04-03 21:27:26 -0600

    swapped to dotenv
    Changed environmental variables to use dotenv instead of system variables

--- wotd_pandas.py
+++ wotd_pandas.py
@@ -1,8 +1,11 @@
 
 import random
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv, find_dotenv
 import pandas as pd
 from twilio.rest import Client
+load_dotenv(find_dotenv())
 word_list = []
 count = 0
 
@@ -18,10 +21,10 @@ def load_dictionary():
 def pick_word():
     """pulls words from the csv file, picks one, and sends it"""
     load_dictionary()
-    ACCOUNT_SID = os.getenv('ACCOUNT_SID')
-    AUTH_TOKEN = os.getenv('AUTH_TOKEN')
-    PH_SEND = os.getenv('ph_send')
-    PH_RECEIVE = os.getenv('ph_receive')
+    ACCOUNT_SID = os.environ.get('ACCOUNT_SID')
+    AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
+    PH_SEND = os.environ.get('PH_SEND')
+    PH_RECEIVE = os.environ.get('PH_RECEIVE')
     selection = random.choice(word_list)
     client = Client(ACCOUNT_SID, AUTH_TOKEN)
     client.messages.create(to=PH_RECEIVE, from_=PH_SEND, body=selection)
